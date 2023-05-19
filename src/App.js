60637b2d887dae6fe9e8684a67e228354fd99d63
amrish <amrish01011999@gmail.com>
2023-05-19 17:25:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> Amrish Kumar software engineer.
         </p>
+        <p>Hello All
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
